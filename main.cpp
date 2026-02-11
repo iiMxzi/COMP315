aa7aad1e20e308b19896fa35a13ae58e38b47004
Ayabonga Mazibuko <224011659@stu.ukzn.ac.za>
2026-02-11 17:41:36 +0200

    attempted arithmetic operations and commenting

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
-    cout << "First change to be commited!";
+    cout << "First change to be commited!\n";
+    cout << 67;
+    cout << "\n3 + 3 = ";
+    cout << 3 + 3;
+    cout << "\n3 x 3 = ";
+    cout << 3 * 3;
+    cout << "\n3 / 3 = ";
+    cout << 3 / 3;
     return 0;
 }
