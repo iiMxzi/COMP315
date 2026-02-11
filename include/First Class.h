6a4356fbd2eaa294501e8f1a6a4f83a0ca7aa3a6
Ayabonga Mazibuko <224011659@stu.ukzn.ac.za>
2026-02-11 16:15:33 +0200

    Attempted to make a new class, it didn't work

---
+++ include/First Class.h
@@ -0,0 +1,16 @@
+#ifndef FIRST CLASS_H
+#define FIRST CLASS_H
+
+
+class First Class
+{
+    public:
+        First Class();
+        virtual ~First Class();
+
+    protected:
+
+    private:
+};
+
+#endif // FIRST CLASS_H
